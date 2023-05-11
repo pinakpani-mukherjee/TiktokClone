c16374a186cac863c7398fe6ca2df06e9520069f
Pinakpani Mukherjee <mukherjee.pinakpani14@gmail.com>
2023-05-11 13:53:38 +0900

    added controllers

--- TiktokClone/Resources/SceneDelegate.swift
+++ TiktokClone/Resources/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let window = UIWindow(windowScene: windowScene)
         
-        window.rootViewController = ViewController()
+        window.rootViewController = HomeViewController()
         
         self.window = window
         self.window?.makeKeyAndVisible()
