02c64c63c7965608ede77e06b7e689998155d053
Pinakpani Mukherjee <mukherjee.pinakpani14@gmail.com>
2023-05-11 12:45:54 +0900

    Initial Commit

---
+++ TiktokClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TiktokClone
+//
+//  Created by Pinakpani Mukherjee on 2023/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
